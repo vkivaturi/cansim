c86468b519fc0c3f5ff07e0bb13a2b01b5bca670
vkivaturi <vijay.ivaturi@gmail.com>
2023-05-27 20:07:29 +0530

    Functions for send and receive

---
+++ src/usecase/vcansimsend.c
@@ -0,0 +1,13 @@
+/*
+Purpose - Simulate a network of devices communicating through virtual CAN interfaces.
+Environment is standard Linux with SocketCAN libraries
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../utils/cansendreceive.h"
+
+int main(int argc, char **argv)
+{
+   int i = sendCANMessage("vcan0", "yahoo");
+}
